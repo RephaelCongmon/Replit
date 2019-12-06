b04b75d5ae59586a19538c6956dc5e706fcc1a03
Rephael Congmon <rephcongmon@gmail.com>
2019-12-06 04:26:50 +0000

    Nothing

--- main.cpp
+++ main.cpp
@@ -5,4 +5,27 @@ int main() {
   cout << "Hello World!\n";
   //test
   //something
+
+  string x = "Bob";
+  string y = "Cat";
+  string temp;
+
+  temp = x;
+  x = y; 
+  y = temp; 
+
+  cout << "x = " << x << endl;
+  cout << "y = " << y << endl;
+  
+  //Conditionals:
+  // == : "Is it equal to?"
+  // != : "Is it not equal to?"
+  // <= : "Is it less than or equal to?"
+  // >= : "Is it greather than or equal to?"
+  // || : OR
+  // && : AND
+
+  
+
+
 }
