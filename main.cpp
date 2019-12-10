a2f23e597f7e661c58e8dac903dbcc0fd90f5e40
Rephael Congmon <rephcongmon@gmail.com>
2019-12-10 03:42:53 +0000

    added arrays lesson

--- main.cpp
+++ main.cpp
@@ -2,7 +2,38 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
+
+  // 0001 1001 <---  8 bits = 1 byte.  A single 0 or 1 is called a bit.
+
+  // 0001 1110
+
+  //http://www.asciitable.com/
+  
+  // 0100 0001 = "A"
+
+  int x = 5;
+  
+  //cout << x
+
+  /*
+    sdfsdf
+    sdfsd
+    sdfsdf
+  */
+  
+          //    0  1  2  3  4
+  int arr[5] = {4, 1, 3, 5, 10};
+
+  // Arrays are 0-index based, meaning 0 is the first element
+  
+  //cout << arr[0] << endl;
+
+          //          0      1      2      3         4        5
+  string arr2[5] = {"Bob", "Cat", "Dog", "House", "Floor"};
+
+  cout << arr2[5] << endl;
+
+  //cout << "Hello World!\n";
   //test
   //something
 
@@ -32,17 +63,17 @@ int main() {
     //Code if true goes in here
   //}
 
-  int x = 10;
-  int y = 10;
-  int z = 13;
+  // int x = 10;
+  // int y = 10;
+  // int z = 13;
 
-  // Write a condition that checks if x is less than y
+  // // Write a condition that checks if x is less than y
 
-  if (x == y || y > z){
-    cout << "Yo!" << endl;
-  }
-  else {
-    cout << "Hey!" << endl;
-  }
+  // if (x == y || y > z){
+  //   cout << "Yo!" << endl;
+  // }
+  // else {
+  //   cout << "Hey!" << endl;
+  // }
 
 }
